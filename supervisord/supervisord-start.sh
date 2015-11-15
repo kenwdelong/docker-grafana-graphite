da8ae42ed949593072da8bb94370171dc8b7b6b0
Ken DeLong <kenwdelong@yahoo.com>
2015-11-15 14:41:17 -0800

    ah yes, a new dashboard loader script

--- supervisord/supervisord-start.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-# Make sure webapp be there in Graphite's log directory,
-# if external volume is used; otherwise, Graphite won't start.
-mkdir -p /opt/graphite/storage/log/webapp
-
-/usr/bin/supervisord
